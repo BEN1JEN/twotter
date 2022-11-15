b01fdcd7af8e002d4aab410779d72229d64c90d3
BEN1JEN <benpheard@gmail.com>
2022-11-14 21:53:24 -0700

    Start to work on database

--- server/src/database.rs
+++ server/src/database.rs
@@ -1,7 +1,7 @@
 use std::path::{Path, PathBuf};
 
 use color_eyre::eyre::Result;
-use surrealdb::Datastore;
+use surrealdb::{Datastore, Session};
 use tokio::fs;
 
 pub struct Database {
@@ -9,11 +9,15 @@ pub struct Database {
 	files_path: PathBuf,
 }
 
+const INIT_QUERIES: &str = include_str!(setup.surreal);
+
 impl Database {
 	pub async fn open<P: AsRef<Path>>(path: P) -> Result<Database> {
 		let files_path = path.as_ref().join("files");
 		fs::create_dir_all(&files_path).await?;
 		let database = Datastore::new(&format!("file://{}", path.as_ref().display())).await?;
+		let res = database.execute("SELECT id FROM info:info WHERE valid == true AND version == 1", &Session::for_kv(), None, false).await?;
+		if !matches!(&res, &[]) {}
 		Ok(Database {
 			database,
 			files_path,
